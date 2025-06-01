1a4c938d42c37f4e025def151980c4dbd0d93fb0
Isael Santos <isael@offertech.com.br>
2025-05-31 23:25:10 -0300

    Updates base URL for application deployment
    The application's base URL is updated to ensure correct
    asset loading and routing in the deployed environment.
    The change facilitates deployment to the root domain.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/istech2/",
+  base: "/",
   plugins: [react()],
 })
 // https://github.com/ISMendys/istech2.git
